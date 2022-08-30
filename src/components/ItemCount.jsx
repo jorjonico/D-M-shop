55fe5d0e3a2251a4dc39dc1ca824f905e7c46a59
Jorge Matinez <jorjonicodcv@gmail.com>
2022-08-30 20:45:36 -0300

    con onadd

--- src/components/ItemCount.jsx
+++ src/components/ItemCount.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import Button from 'react-bootstrap/Button';
-const ItemCount = ({stock, start, mensaje}) => {
-    const [agregar, setAgregar] = useState(start); 
+const ItemCount = ({stock, initial, onAdd}) => {
+    const [agregar, setAgregar] = useState(initial); 
 
     const handleClick = () => {
         if(agregar < stock){
@@ -10,7 +10,7 @@ const ItemCount = ({stock, start, mensaje}) => {
     }
 
     const restaClick = () => {
-        if(agregar > start){
+        if(agregar > initial){
             setAgregar(agregar-1)
         }
     }
@@ -22,7 +22,7 @@ const ItemCount = ({stock, start, mensaje}) => {
             <Button variant="outline-dark" size="sm" className='m-2' onClick={handleClick}>+</Button> 
             <h3>{agregar}</h3>
             <Button variant="outline-dark" size="sm" className='m-2' onClick={restaClick}>-</Button>
-            <Button variant="dark" size="sm" className='m-2' onClick={mensaje}>Agregar al carrito</Button>
+            <Button variant="dark" size="sm" className='m-2' onClick={onAdd}>Agregar al carrito</Button>
         </div>
     );
 }
--- src/components/ItemListContainer.jsx
+++ src/components/ItemListContainer.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ItemCount from "./ItemCount";
 const ItemListContainer = ({greeting}) => {
-    let mensaje = () =>{
+    let onAdd = () =>{
         if (ItemCount.number !== 0) {
             alert(`Usted a cargado ${ItemCount.number} productos al carrito`)
         }
@@ -11,8 +11,8 @@ const ItemListContainer = ({greeting}) => {
         <h4 className="p-5">{greeting}</h4>
         <ItemCount 
         stock={5} 
-        start={1} 
-        mensaje={mensaje}/>
+        initial={1} 
+        onAdd={onAdd}/>
         </>
     );
 }
