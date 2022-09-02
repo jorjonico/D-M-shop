2790974014ac6c384549663c1d1b93b1ab0d5d88
Jorge Matinez <jorjonicodcv@gmail.com>
2022-09-02 14:22:55 -0300

    cambio

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -1,12 +1,20 @@
 import React from "react";
-
+import Card from 'react-bootstrap/Card';
+import ListGroup from 'react-bootstrap/ListGroup';
 function Item({producto}) {
     return(
-        <div>
-        <h1>{producto.nombre}</h1>
-        <p>{producto.color}</p>
-        <h4>$ {producto.precio}</h4>
-        </div>
+        <Card style={{ width: '18rem' }} className="m-3">
+        <Card.Img variant="top" src={producto.imagen} />
+        <Card.Body>
+        <Card.Title>{producto.nombre}</Card.Title>
+        <Card.Text>Color: {producto.color} | Género: {producto.genero}</Card.Text>
+        <ListGroup className="list-group-flush">
+        <ListGroup.Item>Edad: {producto.edad}</ListGroup.Item>
+        <ListGroup.Item>Stock: {producto.stock}</ListGroup.Item>
+        </ListGroup>
+        <h4>Precio: ${producto.precio}</h4>
+        </Card.Body>
+        </Card>
     )
 }
 export default Item
