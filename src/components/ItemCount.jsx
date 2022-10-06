dec9c1521677060d1d53c4d93da327a2de460a1c
Jorge Matinez <jorjonicodcv@gmail.com>
2022-10-06 10:12:17 -0300

    cambio bot

--- src/components/ItemCount.jsx
+++ src/components/ItemCount.jsx
@@ -23,9 +23,9 @@ const ItemCount = ({stock, initial, onAdd}) => {
     }
     return (
         <div className="container d-flex align-items-center">
-            <Button variant="outline-dark" size="sm" className='m-2' onClick={handleClick}>+</Button> 
+            <Button variant="outline-dark" size="sm" className='m-2 px-3' onClick={restaClick}>-</Button>
             <h3>{qty}</h3>
-            <Button variant="outline-dark" size="sm" className='m-2' onClick={restaClick}>-</Button>
+            <Button variant="outline-dark" size="sm" className='m-2 px-3' onClick={handleClick}>+</Button>
             <Button variant="dark" size="sm" className='m-2' onClick={()=> onAdd(qty)}>Cargar al carrito</Button>
         </div>
     );
